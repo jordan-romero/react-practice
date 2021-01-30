1a9818104e09066e23901b9243bee781c0536786
jordles113 <jordan.t.romero@gmail.com>
2021-01-30 16:11:40 -0600

    adds inline styling for btn

--- src/App.js
+++ src/App.js
@@ -30,10 +30,23 @@ export default class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi Im a React App</h1>
-        <button onClick={this.switchNameHandler.bind(this, 'Meks')}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={this.switchNameHandler.bind(this, 'Meks')}>
+          Switch Name
+        </button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Person.css'
 
 const Person = (props) => {
     return (
-        <div>
+        <div className='Person'>
             <p onClick={props.onClick}>My name is {props.name} and I am {props.age} years old!</p> 
             <p>{props.children}</p>
             <input type='text' onChange={props.onChange} value={props.name}/>
