6fe327bd210a97e6632fc5d459cb629864c46964
jordles113 <jordan.t.romero@gmail.com>
2021-01-30 15:27:55 -0600

    wires up onClick

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react'
 import Person from './Person/Person'
 
 export default class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: 30 },
+      { name: 'Jordan', age: 31 },
+      { name: 'Tracie', age: 40 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('was clicked')
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi Im a React App</h1>
-        <Person name='Max' age='30'/>
-        <Person name='Jordan' age='31'/>
-        <Person name='Tracie' age='40'/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     )
   }
