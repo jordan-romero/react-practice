4a485dc54ff45c12ae0c9d26577bbc8c7cf68bc0
jordles113 <jordan.t.romero@gmail.com>
2021-01-31 18:04:18 -0600

    creates togglePersonsHandler

--- src/App.js
+++ src/App.js
@@ -29,6 +29,10 @@ export default class App extends Component {
     })
   }
 
+  togglePersonsHandler = () => {
+    
+  }
+
   render() {
 
     const style = {
@@ -44,22 +48,24 @@ export default class App extends Component {
         <h1>Hi Im a React App</h1>
         <button 
           style={style}
-          onClick={this.switchNameHandler.bind(this, 'Meks')}>
+          onClick={this.togglePersonsHandler}>
           Switch Name
         </button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-        />
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          onChange={this.nameChangedHandler}
-        />
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}
-        />
+        <div> 
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}
+          />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            onChange={this.nameChangedHandler}
+          />
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}
+          />
+        </div>
       </div>
     )
   }
