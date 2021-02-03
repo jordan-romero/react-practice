0c999e1a31a9f95e9fc9d89c794bcb3ae90930ce
jordles113 <jordan.t.romero@gmail.com>
2021-02-03 12:47:26 -0600

    working with lifecycle methods

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import './Person.css'
-import styled from 'styled-components'
+
 
 class Person extends Component {
     render() {
         return (
-            <StyledDiv>
-                 <p onClick={props.onClick}>My name is {props.name} and I am {props.age} years old!</p> 
-                 <p>{props.children}</p>
-                 <input type='text' onChange={props.onChange} value={props.name}/>
-             </StyledDiv>
+            <div>
+                 <p onClick={this.props.onClick}>My name is {this.props.name} and I am {this.props.age} years old!</p> 
+                 <p>{this.props.children}</p>
+                 <input type='text' onChange={this.props.onChange} value={this.props.name}/>
+             </div>
              )
     }
     
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react'
 import Person from './Person/Person'
 
 class Persons extends Component {
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("get derived state from props")
+    return state
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return true
+  }
   render (){
-    props.persons.map((person, index) => {
+    return this.props.persons.map((person, index) => {
       return <Person {...person} 
-      onClick={() => props.onClick(index)} 
+      onClick={() => this.props.onClick(index)} 
       key={person.id}
-      onChange={(event) => props.onChange(event, person.id)}/>
+      onChange={(event) => this.props.onChange(event, person.id)}/>
     })
   }
 }
