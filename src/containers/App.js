feba7aff5eb443e57b73f30d475a8671100b39e0
jordles113 <jordan.t.romero@gmail.com>
2021-02-05 19:44:12 -0600

    uses prevState

--- src/containers/App.js
+++ src/containers/App.js
@@ -62,7 +62,10 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons, changeCounter:  this.state.changeCounter + 1});
+    this.setState((prevState, props) => {
+      return { persons: persons, 
+        changeCounter:  prevState + 1}
+    });
   };
 
   deletePersonHandler = personIndex => {
